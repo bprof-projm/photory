a5b0d9da5975d82c19809b2efe420a6df7626b48
Gadacsi Akos <akosgadacsi99@gmail.com>
2021-02-28 20:10:57 +0100

    Photory model/data updated to dotnet 5, dbcontext created

--- WebApi/PhotoryData/PhotoryDbContext.cs
+++ WebApi/PhotoryData/PhotoryDbContext.cs
@@ -1,31 +1,34 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using PhotoryModels;
+using System.Linq;
 
 namespace PhotoryData
 {
     public class PhotoryDbContext:DbContext
     {
+        private readonly string ConnectionStrinPassword;
 
-        public PhotoryDbContext()
+        public PhotoryDbContext(string ConnectionStrinPassword)
         {
-            
+            this.ConnectionStrinPassword = ConnectionStrinPassword;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                var builder = new SqlConnectionStringBuilder("Server=tcp:photory.database.windows.net,1433;Initial Catalog=DataPhotory;Persist Security Info=False;User ID=akosgdcsi;Password=Akos1999;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;");
-               
-                optionsBuilder.UseSqlServer();
+                var builder = new SqlConnectionStringBuilder("Server=tcp:photory.database.windows.net,1433;Initial Catalog=DataPhotory;Persist Security Info=False;User ID=akosgdcsi;Password={ConnectionStrinPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder
             }
         }
 
 
         public virtual DbSet<PhotoryModels.User> Users { get; set; }
-        public virtual DbSet<> OneVote { get; set; }
-        public virtual DbSet<Models.AllVotes> AllVotes { get; set; }
+        public virtual DbSet<PhotoryModels.Group> Groups { get; set; }
+        public virtual DbSet<PhotoryModels.Photo> Photos { get; set; }
+        public virtual DbSet<PhotoryModels.Comment> Comments { get; set; }
     }
 }
