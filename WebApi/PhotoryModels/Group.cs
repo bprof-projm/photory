dadb450697f23adc649dadb8ac5312ab5b7401e8
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-02-28 19:19:36 +0100

    Group Model created

---
+++ WebApi/PhotoryModels/Group.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoryModels
+{
+    class Group
+    {
+
+        public string GroupName { get; set; }
+
+        public string GroupAdminID { get; set; }
+
+        public int Age { get; set; }
+
+        public List<string> PendingUserIDList { get; set; }
+
+        public List<string> PhotoIDList { get; set; }
+
+        //public List<string> MembersID { get; set; }
+
+
+    }
+}
