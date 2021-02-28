6459aacd81ecf0662617ea5ed8b38bb5973a48e0
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-02-28 19:11:44 +0100

    UserModel Created

---
+++ WebApi/PhotoryModels/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PhotoryModels
+{
+
+    public enum UserAccess 
+    {
+        RegularUser,GroupAdmin,Admin
+    }
+
+    public class User
+    {
+        public string Email { get; set; }
+
+        public string FullName { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Password { get; set; }
+        public UserAccess UserAccess { get; set; }
+
+
+    }
+}
