fdbdcc828c559b0753f0d256ba32bbb762e11c76
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2021-03-17 21:10:02 +0100

    Navbar added to Home component

--- photoryapp/src/App.js
+++ photoryapp/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Register from './Components/Register/Register';
 import Home from './Components/Home/Home';
+import Browser from './Components/Browser/Browser';
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={Home}></Route>
       <Route exact path='/register' component={Register}></Route>
+      <Route exact path='/browser' component={Browser}></Route>
     </Switch> 
   );
 }
