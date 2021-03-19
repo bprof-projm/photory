e6b8f787e707359d607933be5d99c9c961e415c0
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-03-19 19:06:04 +0100

    UserGroup model created

---
+++ Photory/PhotoryModels/UserGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoryModels
+{
+    public class UserGroup
+    {
+        public string UserName { get; set; }
+        public virtual User User { get; set; }
+
+        public string GroupName { get; set; }
+
+        public virtual Group Group { get; set; }
+
+
+
+
+    }
+}
