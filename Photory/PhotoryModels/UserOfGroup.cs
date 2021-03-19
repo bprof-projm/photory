a82ea1486739065e40f3e16bf7f9d3d5446b6cb5
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-03-19 19:06:30 +0100

    UserOfGroup model created

---
+++ Photory/PhotoryModels/UserOfGroup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PhotoryModels
+{
+    public class UserOfGroup
+    {
+        [Key]
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual User User { get; set; }
+
+
+        public bool IsPending { get; set; }
+
+
+        
+        public string GroupName { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual Group Group { get; set; }
+
+    }
+}
