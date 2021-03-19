a55f5803707d5cfbc4afce742431233d2576955b
Gadacsi Akos <akosgadacsi99@gmail.com>
2021-03-19 20:10:42 +0100

    controller added

---
+++ Photory/Photory/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoryLogic.Classes;
+using PhotoryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Photory.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class UserController : ControllerBase
+    {
+        UserLogic userlogic;
+
+        public UserController(UserLogic userlogic)
+        {
+            this.userlogic = userlogic;
+        }
+
+
+        [HttpPost]
+        public void CreateUser([FromBody] User user)
+        {
+            userlogic.CreateUser(user);
+        }
+
+
+        [HttpGet]
+        public IEnumerable<User> GetAllUser()
+        {
+            return userlogic.GetAllUser();
+        }
+
+    }
+}
+
