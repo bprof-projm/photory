5e69558b3d6d934d62544324df2e2aaa19f10dd5
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-03-21 18:04:51 +0100

    UserPhotoController done

---
+++ Photory/Photory/Controllers/UserPhotoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoryLogic.Classes;
+using PhotoryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Photory.Controllers
+{
+
+    [ApiController]
+    [Route("{UserPhoto}")]
+    public class UserPhotoController
+    {
+
+        UserLogic userlogic;
+
+        public UserPhotoController(UserLogic userlogic)
+        {
+            this.userlogic = userlogic;
+        }
+
+
+        [HttpPost]
+        public void AddPhoto([FromBody] Photo p)
+        {
+            userlogic.AddPhoto(p);
+
+        }
+
+        [HttpDelete("{id}")]
+        public void DeletePhoto(string id)
+        {
+            userlogic.DeletePhoto(id);
+        }
+
+
+
+
+    }
+}
