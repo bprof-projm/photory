8cefac40b03b498016348bf9cc780389d40ec16c
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-03-22 21:22:33 +0100

    IGroupRepository created

---
+++ Photory/PhotoryRepository/Interfaces/IGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using PhotoryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoryRepository.Interfaces
+{
+   public  interface IGroupRepository :IRepository<Group>
+    {
+    }
+}
