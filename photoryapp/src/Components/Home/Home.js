1fb41deb82eb40eda6b31533b20c48a79b849a5a
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2021-03-23 13:54:57 +0100

    Navbar added to Home component

--- photoryapp/src/Components/Home/Home.js
+++ photoryapp/src/Components/Home/Home.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 
 class Home extends Component{
     render(){
         return(
             <div>
-                <p>Home Page</p>
-                <Link to="/register">
-                    <button>
-                        Register
-                    </button>
-                </Link>
+                <Navbar text1="Browser" link1="/browser" text2="Something" link2="#" text3='Register' link3="/register"/>
             </div>
         );
     }
