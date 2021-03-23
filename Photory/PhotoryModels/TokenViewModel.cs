20546cf252967690d2cf92f0a5331a841eca9abc
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-03-23 22:31:31 +0100

    Viewmodel classes created for auth

---
+++ Photory/PhotoryModels/TokenViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoryModels
+{
+    public class TokenViewModel
+    {
+        public string Token { get; set; }
+
+        public DateTime Expiration { get; set; }
+
+    }
+}
