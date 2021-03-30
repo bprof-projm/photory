853aae26eb03822dc196718cbb7d8bb19377178c
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-03-30 21:09:36 +0200

    IContentLogic created

---
+++ Photory/PhotoryLogic/Interfaces/IContentLogic.cs
@@ -0,0 +1,20 @@
+﻿using PhotoryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoryLogic.Interfaces
+{
+    public interface IContentLogic
+    {
+         IQueryable<Comment> GetAllCommentsFromPhoto(string PhotoID);
+
+         Photo GetOnePhoto(string photoid);
+
+        IQueryable<Photo> GetPhotosFromGroup(string GroupID);
+
+        IQueryable<Group> GetAllGroup();
+    }
+}
