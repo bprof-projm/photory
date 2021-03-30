9bc683d8682d9b2d4b31fdcf39533cc60eb5e1af
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-03-30 21:03:17 +0200

    IPhotoRepository created

---
+++ Photory/PhotoryRepository/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,16 @@
+﻿using PhotoryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoryRepository.Interfaces
+{
+    public interface IPhotoRepository
+    {
+
+        Photo GetOnePhoto(string PhotoID);
+         IQueryable<Comment> GetAllCommentsFromPhoto(string PhotoID);
+    }
+}
