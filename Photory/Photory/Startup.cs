850d645676b20a2bf0cc2fdca0579f8cda03905c
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-07 19:28:12 +0200

    Swagger services modified with bearer token auth

--- Photory/Photory/Startup.cs
+++ Photory/Photory/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PhotoryData;
 using PhotoryLogic.Classes;
 using PhotoryModels;
@@ -44,9 +45,33 @@ namespace WebApi
             services.AddTransient<IUserOfGroupRepository, UserOfGroupRepository>();
             services.AddTransient<IGroupRepository, GroupRepository>();
             services.AddTransient<IPhotoRepository, PhotoRepository>();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                // configure SwaggerDoc and others
 
-            services.AddDbContext<PhotoryDbContext>();
+                // add JWT Authentication
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "JWT Authentication",
+                Description = "Enter JWT Bearer token **_only_**",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer", // must be lower case
+                BearerFormat = "JWT",
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+            {securityScheme, new string[] { }}
+            });
+            });
+
+                services.AddDbContext<PhotoryDbContext>();
 
             //services.AddCors(options =>
             //{
