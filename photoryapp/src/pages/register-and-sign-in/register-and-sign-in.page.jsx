3f448bc30cd56444d85f59f8158dda637513c276
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2021-04-21 01:41:42 +0200

    main page modified

--- photoryapp/src/pages/register-and-sign-in/register-and-sign-in.page.jsx
+++ photoryapp/src/pages/register-and-sign-in/register-and-sign-in.page.jsx
@@ -6,12 +6,13 @@ import FacebookLogin from 'react-facebook-login';
 import { setMode } from '../../functions.js';
 
 import './register-and-sign-in.styles.scss';
+import axios from 'axios';
 
 class RegisterAndSignInPage extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            withServer: true
+            withServer: true            
         };
         this.history = this.props.history;        
     }
@@ -43,7 +44,55 @@ class RegisterAndSignInPage extends React.Component{
 
     responseFacebook = (response) => {
         console.log(response);
-    }
+
+        const data = {
+            accessToken: response.accessToken
+        }
+        axios.post('/ExternalAuth', data)
+        .then(res => {
+            console.log(res);  
+            this.setState({ token: res.data?.token });
+            console.log(this.state.token);  
+            this.props.setToken(res.data?.token); 
+            var decoded = jwt_decode(this.state.token);
+            console.log(decoded);
+            var userId = '';
+            var role = '';
+            Object.keys(decoded).forEach(function (key) {
+                let res = key.split("/");
+                if (res.length > 1) {
+                    if (res[res.length - 1] === 'nameidentifier') {
+                      userId = decoded[key];                       
+                    }     
+                    else if (res[res.length - 1] === 'role') {
+                      if (decoded[key] === 'Customer'){
+                        role = 'User';
+                      }
+                      else {
+                        role = decoded[key];
+                      }                                           
+                    }              
+                }
+            });
+                        
+            const headers={
+                'Authorization': 'Bearer ' + this.state.token
+            };
+            axios.get(`/${role}/${userId}`, { headers: headers })
+            .then(res => {
+                console.log(res.data);
+                this.props.setCurrentUser(res.data);
+                this.props.setNewPassword('');
+                this.history.push("/groups");   
+            })
+            .catch(error => {
+                console.log(error);
+            })               
+        })
+        .then(error => {
+            console.log(error);
+        })
+    }   
 
     render(){
         return(
@@ -57,7 +106,7 @@ class RegisterAndSignInPage extends React.Component{
                     <button className='rs-mode' onClick={this.handleModChange}>On</button>
                 </div>
                 <FacebookLogin
-                    appId="1088597931155576"
+                    appId="287947399346523"
                     autoLoad={true}
                     fields="name,email,picture"                    
                     callback={this.responseFacebook} />
@@ -66,4 +115,10 @@ class RegisterAndSignInPage extends React.Component{
     }
 
 }
-export default withRouter(RegisterAndSignInPage);
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user)),
+    setNewPassword: password => dispatch(setNewPassword(password)),
+    setToken: token => dispatch(setToken(token))
+  });
+  
+  export default withRouter(connect(mapStateToProps, mapDispatchToProps)(RegisterAndSignInPage));
