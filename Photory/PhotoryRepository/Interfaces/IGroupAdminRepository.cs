27569109568eb17e43761051235748ad300a375b
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-22 19:49:43 +0200

    IGroupAdminRepository formatted

--- Photory/PhotoryRepository/Interfaces/IGroupAdminRepository.cs
+++ Photory/PhotoryRepository/Interfaces/IGroupAdminRepository.cs
@@ -1,21 +1,13 @@
 ﻿using PhotoryModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoryRepository.Interfaces
 {
     public interface IGroupAdminRepository : IRepository<User>
     {
         //ACCEPT USER
-        public void AcceptUser(string userID, string GroupID);
+        void AcceptUser(string userID, string GroupID);
 
         //DENY USER
-        public void DenyUser(string userID, string GroupID);
-
-
-
+        void DenyUser(string userID, string GroupID);
     }
 }
