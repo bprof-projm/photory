dc6fa394def976784f622ac9f000c9dd1028ca93
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-22 19:50:25 +0200

    IPhotoRepo formatting updated

--- Photory/PhotoryRepository/Interfaces/IPhotoRepository.cs
+++ Photory/PhotoryRepository/Interfaces/IPhotoRepository.cs
@@ -1,18 +1,14 @@
 ﻿using PhotoryModels;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoryRepository.Interfaces
 {
     public interface IPhotoRepository
     {
-
         Photo GetOnePhoto(string PhotoID);
-         IQueryable<Comment> GetAllCommentsFromPhoto(string PhotoID);
-        Photo GetOneRescaledPhoto(string PhotoID);
 
+        IQueryable<Comment> GetAllCommentsFromPhoto(string PhotoID);
+
+        Photo GetOneRescaledPhoto(string PhotoID);
     }
 }
