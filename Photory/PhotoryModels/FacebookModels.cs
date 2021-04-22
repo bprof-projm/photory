8beafd3f4bc51cf627f0016fafdcba89df6126b5
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-22 19:45:12 +0200

    FaceBookModels formatting fixed

--- Photory/PhotoryModels/FacebookModels.cs
+++ Photory/PhotoryModels/FacebookModels.cs
@@ -1,29 +1,19 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoryModels
 {
-
-
     public class FacebookUserData
     {
         public long Id { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public string Gender { get; set; }
- 
+
         public DateTime Birthdate { get; set; }
-      
+
         public FacebookPictureData Picture { get; set; }
     }
-
-
 
     public class FacebookPictureData
     {
@@ -34,8 +24,10 @@ namespace PhotoryModels
     {
         public int Height { get; set; }
         public int Width { get; set; }
+
         [JsonProperty("is_silhouette")]
         public bool IsSilhouette { get; set; }
+
         public string Url { get; set; }
     }
 
@@ -43,12 +35,16 @@ namespace PhotoryModels
     {
         [JsonProperty("app_id")]
         public long AppId { get; set; }
+
         public string Type { get; set; }
         public string Application { get; set; }
+
         [JsonProperty("expires_at")]
         public long ExpiresAt { get; set; }
+
         [JsonProperty("is_valid")]
         public bool IsValid { get; set; }
+
         [JsonProperty("user_id")]
         public long UserId { get; set; }
     }
@@ -62,6 +58,7 @@ namespace PhotoryModels
     {
         [JsonProperty("token_type")]
         public string TokenType { get; set; }
+
         [JsonProperty("access_token")]
         public string AccessToken { get; set; }
     }
