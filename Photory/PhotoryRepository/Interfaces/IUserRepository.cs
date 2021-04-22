306da4f30d5d9154c23282b57f3fd60fbcb636f8
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-22 19:51:45 +0200

    IUserRepository code formatted

--- Photory/PhotoryRepository/Interfaces/IUserRepository.cs
+++ Photory/PhotoryRepository/Interfaces/IUserRepository.cs
@@ -1,35 +1,27 @@
 ﻿using PhotoryModels;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace PhotoryRepository.Interfaces
 {
     public interface IUserRepository : IRepository<User>
     {
-
         //ADD PHOTO
-        public void AddPhoto(Photo p);
+         void AddPhoto(Photo p);
 
         //ADD COMMENT
-        public void AddComment(Comment m);
+        void AddComment(Comment m);
 
         //REQUEST JOIN
-        public void RequestJoin(string userid,string GroupID);//??userid
+         void RequestJoin(string userid, string GroupID);//??userid
 
         //DELETE PHOTO
-        public void DeletePhoto(string PhotoID);
-
+         void DeletePhoto(string PhotoID);
 
         //DELETE COMMENT
-        public void DeleteComment(string CommentID);
+         void DeleteComment(string CommentID);
 
         IQueryable<Comment> GetAllCommentsFromPhoto(string photoID);
 
         void LeaveGroup(string userID, string GroupID);
-
-
-
     }
 }
