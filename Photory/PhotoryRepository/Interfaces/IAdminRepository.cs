b294774722767e3b19378c4a9af5d395992c1f02
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-22 19:49:10 +0200

    IAdminrepo formatting fixed

--- Photory/PhotoryRepository/Interfaces/IAdminRepository.cs
+++ Photory/PhotoryRepository/Interfaces/IAdminRepository.cs
@@ -1,24 +1,13 @@
 ﻿using PhotoryModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoryRepository.Interfaces
 {
-    public interface IAdminRepository :IRepository<User>
+    public interface IAdminRepository : IRepository<User>
     {
-
         //CREATE GROUP
-        public void CreateGroup(Group group);//CreateGroup();
+        void CreateGroup(Group group);//CreateGroup();
 
         //ADD-MEMBER TO GROUP
-        public void AddMember(string userID, string GroupID);
-
-       
-
-       
-
+        void AddMember(string userID, string GroupID);
     }
 }
